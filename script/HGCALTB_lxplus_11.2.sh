8421334eb4c7089c4dc82d687b3a0c1e790da82f
Lorenzo Pezzotti <lorenzo.pezzotti01@gmail.com>
2024-01-16 17:42:09 +0100

    Use Geant4-11.2 in corresponding script
    Script for lxplus usage on Centos7 and Geant4-11.2.

--- script/HGCALTB_lxplus_11.2.sh
+++ script/HGCALTB_lxplus_11.2.sh
@@ -2,7 +2,7 @@
 
 export G4GCC_VERSION="10"
 export G4GCC_PLATFORM="x86_64-centos7-gcc10-opt"
-export GEANT4_VERSION="11.1"
+export GEANT4_VERSION="11.2"
 export GEANT4_PLATFORM="x86_64-centos7-gcc10-optdeb-MT"
 export GCC_VERSION="10"
 export GCC_PLATFORM="x86_64-centos7-gcc10-opt"
