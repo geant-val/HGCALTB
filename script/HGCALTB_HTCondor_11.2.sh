f7ab1dfd785c36b27b34bb311ca1d7ad58114a1f
Lorenzo Pezzotti <lorenzo.pezzotti01@gmail.com>
2024-01-17 09:53:05 +0100

    Modify lxplus and HTCondor scripts for alma9
    Modify scripts for execution on lxplus using G4-11.2 and the new default
    OS on lxplus, alma9 (el9). Tested for usage on lxplus and HTCondor.

--- script/HGCALTB_HTCondor_11.2.sh
+++ script/HGCALTB_HTCondor_11.2.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
-export G4GCC_VERSION="10"
-export G4GCC_PLATFORM="x86_64-centos7-gcc10-opt"
+export G4GCC_VERSION="12"
+export G4GCC_PLATFORM="x86_64-el9-gcc12-opt"
 export GEANT4_VERSION="11.2"
-export GEANT4_PLATFORM="x86_64-centos7-gcc10-optdeb-MT"
-export GCC_VERSION="10"
-export GCC_PLATFORM="x86_64-centos7-gcc10-opt"
+export GEANT4_PLATFORM="x86_64-el9-gcc12-optdeb-MT"
+export GCC_VERSION="12"
+export GCC_PLATFORM="x86_64-el9-gcc12-opt"
 
 source ./HGCALTB_cvmfs_setup.sh
